55d053ea5699aa98d454b3d898806f5a4e14414d
wesleyshipley <Temuco24!>
2021-05-28 22:15:04 -0400

    added flask job scheduler

--- app.py
+++ app.py
@@ -9,6 +9,31 @@ import json
 import uuid
 from redis import Redis
 from rq.registry import StartedJobRegistry
+from apscheduler.jobstores.sqlalchemy import SQLAlchemyJobStore
+from flask_apscheduler import APScheduler
+
+
+class Config:
+    """App configuration."""
+
+    JOBS = [
+        {
+            "id": "job1",
+            "func": "advanced:job1",
+            "args": (1, 2),
+            "trigger": "interval",
+            "seconds": 10,
+        }
+    ]
+
+    SCHEDULER_JOBSTORES = {"default": SQLAlchemyJobStore(url="sqlite://")}
+
+    SCHEDULER_EXECUTORS = {"default": {"type": "threadpool", "max_workers": 20}}
+
+    SCHEDULER_JOB_DEFAULTS = {"coalesce": False, "max_instances": 3}
+
+    SCHEDULER_API_ENABLED = True
+
 
 app = Flask(__name__)
 
@@ -25,13 +50,12 @@ def running_jobs():
     registry = StartedJobRegistry('default', connection=redis_conn)
     running_job_ids = registry.get_job_ids()  # Jobs which are exactly running.
     expired_job_ids = registry.get_expired_job_ids()
-    info = {"running_jobs": running_job_ids, "expired_jobs" : expired_job_ids}
+    info = {"running_jobs": running_job_ids, "expired_jobs": expired_job_ids}
     return json.dumps(info)
 
 
 @app.route("/results/<job_key>", methods=['GET'])
 def get_results(job_key):
-
     job = Job.fetch(job_key, connection=conn)
     if job.is_finished:
         result = job.result
@@ -63,5 +87,19 @@ def get_counts():
     return job.get_id()
 
 
+def job1(var_one, var_two):
+    """Demo job function.
+    :param var_two:
+    :param var_two:
+    """
+    print(str(var_one) + " " + str(var_two))
+
+
 if __name__ == "__main__":
+    app = Flask(__name__)
+    app.config.from_object(Config())
+
+    scheduler = APScheduler()
+    scheduler.init_app(app)
+    scheduler.start()
     app.run(host='0.0.0.0')
